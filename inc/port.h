1a515d42aca63d7f64ca59f3251d67a510bc67f6
nikhiljose21 <johndoe@example.com>
2021-04-26 22:43:54 +0530

    multilevel coding

---
+++ inc/port.h
@@ -0,0 +1,16 @@
+
+
+
+#ifndef _PORT__H_
+    #define _PORT__h_
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ * @brief port definitions
+ * 
+ */
+void port();
+
+#endif
--- main.c
+++ main.c
@@ -9,18 +9,14 @@
  * 
  */
 
-#include <avr/io.h>
-#include <util/delay.h>
+
+#include "port.h"
 
 
 int main(void)
 {
 
-    DDRB |= (1<<PB0); // set B0 for led
-    DDRD &=~ (1<<PD0); // clear bit
-    PORTD |= (1<<PD0); // SET BIT
-    DDRC &=~ (1<<PC0); // clear bit
-    PORTC |= (1<<PC0); // set bit
+    port();
 
     while(1)
     {
